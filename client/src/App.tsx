e6bf3fac8cfb4246f0abd5008bee9e77ffb5ae94
devwithmpho <mphosomo07@gmail.com>
2025-08-08 14:13:01 +0200

    chore: setup React Router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import NotFoundPage from './Pages/NotFoundPage';
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="*" element={<NotFoundPage />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
